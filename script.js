ff7ec02aa5abcccd53a82d02496bad14aa47611e
Henero <shinjinocode@gmail.com>
2023-06-11 21:28:14 +0200

    add index.html and script.js, create a randomChoice function to play against a machine

---
+++ script.js
@@ -0,0 +1,13 @@
+let choice = ["paper", "rock", "scissor"];
+
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * choice.length + 1);
+
+  if (computerChoice === 1) {
+    return "Paper";
+  } else if (computerChoice === 2) {
+    return "Rock";
+  } else {
+    return "Scissor";
+  }
+}
