3f98957806e4f866c25de127d4055f4d6e350364
Henero <shinjinocode@gmail.com>
2023-06-28 15:51:31 +0200

    set up the game

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-let choice = ["paper", "rock", "scissor"];
-
 function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * choice.length + 1);
+  const pick = ["rock", "paper", "scissors"];
+  return pick[Math.floor(Math.random() * pick.length)];
+}
 
-  if (computerChoice === 1) {
-    return "Paper";
-  } else if (computerChoice === 2) {
-    return "Rock";
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `This is a tie, both have chose ${playerSelection}\nPlayer Score : ${playerScore}\nComputer Score : ${computerScore}`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++;
+    return `You win ${playerSelection} beats ${computerSelection}\nplayerScore : ${playerScore}\ncomputerScore : ${computerScore}`;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
+    return `You win ${playerSelection} beats ${computerSelection}\nplayerScore : ${playerScore}\ncomputerScore : ${computerScore}`;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
+    return `You win ${playerSelection} beats ${computerSelection}\nplayerScore : ${playerScore}\ncomputerScore : ${computerScore}`;
   } else {
-    return "Scissor";
+    computerScore++;
+    return `You loose, ${computerSelection} beats ${playerSelection}\nplayerScore : ${playerScore}\ncomputerScore : ${computerScore}`;
   }
 }
+
+function game() {
+  let count = 0;
+  while (count < 5) {
+    const playerSelection = prompt(
+      "Choose between Rock, Paper or Scissors"
+    ).toLowerCase();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    count++;
+  }
+  if (playerScore > computerScore) {
+    return `You win`;
+  } else {
+    return `You loose`;
+  }
+}
+
+console.log(game());
